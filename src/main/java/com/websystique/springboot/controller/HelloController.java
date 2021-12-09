3f2305d92c50a38a0936aea5a6ef4546e7236c35
Deepika-tmna <45033433+Deepika-tmna@users.noreply.github.com>
2021-12-09 17:29:19 +0530

    Update HelloController.java

--- src/main/java/com/websystique/springboot/controller/HelloController.java
+++ src/main/java/com/websystique/springboot/controller/HelloController.java
@@ -3,11 +3,13 @@ package com.websystique.springboot.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class HelloController {
 
-	@RequestMapping("/azure-demo")
+	@GetMapping("/azure-demo")
 	String home(ModelMap modal) {
 		modal.addAttribute("title", "Dear Learner");
 		modal.addAttribute("message", "Welcome to SpringBoot");
